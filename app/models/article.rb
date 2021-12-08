679e6ba7ae815f1721fb1773ace1e6ac6b51f1ec
Daria Lasecka <daria.lasecka@htdevelopers.com>
2021-12-08 14:05:31 +0100

    Add comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    has_many :comments, class_name: "comment", foreign_key: "reference_id"
+
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
 end
